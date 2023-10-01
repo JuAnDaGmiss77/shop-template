4862566e9617de598d57f62800166f1aec573184
JuAnDaGmiss77 <juandavidb741@gmail.com>
2023-10-01 00:05:11 -0500

    agrega la funcionalidad para listar los productos desde el store de pinia

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
 import FooterShop from '../components/FooterShop.vue'
 import NavigationMenu from '../components/NavigationMenu.vue'
+import { useProduct } from '../composables/useProduct'
 
+const { products, getProducts } = useProduct()
+
+//get products from fake data
+getProducts()
 
 </script>
 
@@ -9,18 +14,18 @@ import NavigationMenu from '../components/NavigationMenu.vue'
     <NavigationMenu />
 
     <!-- products init-->
-    <main class="grid grid-cols-4 gap-4 container mx-auto mt-6">
+    <main v-for="product in products" :key="product.id" class="grid grid-cols-4 gap-4 container mx-auto mt-6">
 
 
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="rounded-t-lg" src="img/product1.jpg" alt="" />
+                <img class="rounded-t-lg" :src="product.image" alt="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cereal</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ product.name }}</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">$1000</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${{ product.pricing }}</p>
                 <a href="product_detail.html"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Detalle
