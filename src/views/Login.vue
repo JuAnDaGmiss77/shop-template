a5919fd64cb0dbb1f235d37f8c710f4dc74fc924
JuAnDaGmiss77 <juandavidb741@gmail.com>
2023-10-02 21:22:22 -0500

    elimina los datos por defecto de los inputs del login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,14 +16,14 @@ import NavigationMenu from '../components/NavigationMenu.vue'
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
                     <input type="text" name="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        placeholder="ingresa tu usuario..." required value="usuarioPorDefecto">
+                        placeholder="ingresa tu usuario..." required>
                 </div>
                 <div>
                     <label for="password"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
                     <input type="password" name="password" id="password" placeholder="••••••••"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        required value="test">
+                        required>
                 </div>
 
                 <RouterLink :to="{name: 'Administration'}">
